12c175580c66eb1de7179b39230f5684e0cc3cff
bobruisk5050-del <bobruisk5050@gmail.com>
2026-08-22 13:55:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -777,6 +777,8 @@ function initFAQ() {
   });
 }
 
+const BYN_SIGN = `<svg class="byn-sign" viewBox="0 0 360.67 446.4" aria-hidden="true" focusable="false"><path fill="currentColor" d="M475.61,528.84c0-72.5-62.75-131.27-140.16-131.27H227.58V263.37H426v-49.6H178v290h-63.1v49.7H178V660.17h49.54l107.92-.07c77.36,0,140.11-58.77,140.11-131.26Zm-248-25.1V447.1c35.89,0,72.35.07,107.87.07,50,0,90.56,36.57,90.56,81.67s-40.54,81.67-90.56,81.7l-107.87,0V553.44h112.7v-49.7Z" transform="translate(-114.94 -213.77)"/></svg>`;
+
 function initPrices() {
   let day = "weekday";
   let cat = "vrGames";
@@ -818,7 +820,7 @@ function initPrices() {
         return `
         <div class="price-block${r.hit ? " hit" : ""}">
           <div class="pb-time">${r.time}</div>
-          <div class="pb-price">${price}<span class="byn-sign" aria-label="белорусских рублей"></span></div>
+          <div class="pb-price">${price}${BYN_SIGN}</div>
           <a href="tel:+375299993393" class="btn-primary">Забронировать</a>
         </div>`;
       })
